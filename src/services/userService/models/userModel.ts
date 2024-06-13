f53b34db923807a7302e765898011c4cc792c53f
venu123143 <venugopalreddy9493@gmail.com>
2024-06-13 13:44:34 +0530

    models upgraded to v6 from v5 sequilize

--- src/services/userService/models/userModel.ts
+++ src/services/userService/models/userModel.ts
@@ -1,87 +1,107 @@
-import { Model, Sequelize } from 'sequelize';
+import { DataTypes, Model, Optional, Sequelize } from 'sequelize';
 
 export interface IUser {
-    id?: number;
-    fullName: string;
-    accountName: string;
-    userType?: string;
-    profilePicture?: string;
-    coverPhoto?: string;
-    countryCode?: string;
-    phoneNumber: string;
-    theme?: string;
-    password: string;
-    timeZone?: string;
-    lastLogin?: number;
-    createdAt?: Date
-    updatedAt?: Date
-
-}
-export interface UserInstance extends Model<IUser>, IUser { }
-
-const UserModel = (sequelize: Sequelize, DataTypes: any) => {
-    const User = sequelize.define<UserInstance>('users', {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        fullName: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        accountName: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        userType: {
-            type: DataTypes.STRING,
-            defaultValue: 'user'
-        },
-        profilePicture: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        coverPhoto: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        countryCode: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        phoneNumber: {
-            type: DataTypes.STRING,
-            allowNull: true,
-            unique: true,
-        },
-        theme: {
-            type: DataTypes.STRING,
-            defaultValue: 'system'
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        timeZone: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        lastLogin: {
-            type: DataTypes.INTEGER,
-            allowNull: true
-        },
-
-    }, {
-        freezeTableName: true, timestamps: true, indexes: [{
-            unique: false,
-            fields: ['phoneNumber'],
-            name: 'phoneNumber_index'
-        }]
-    });
-
-    return User;
+  id: number;
+  fullName: string;
+  accountName: string;
+  userType: string;
+  profilePicture: string | null;
+  coverPhoto: string | null;
+  countryCode: string | null;
+  phoneNumber: string;
+  theme: string;
+  password: string;
+  timeZone: string | null;
+  lastLogin: number | null;
 }
 
+// These attributes will be optional when calling UserModel.create()
+export interface UserModel extends Optional<IUser, 'id' | 'userType' | 'profilePicture' | 'coverPhoto' | 'countryCode' | 'theme' | 'timeZone' | 'lastLogin'> {}
+
+class User extends Model<IUser, UserModel> implements IUser {
+  public id!: number;
+  public fullName!: string;
+  public accountName!: string;
+  public userType!: string;
+  public profilePicture!: string | null;
+  public coverPhoto!: string | null;
+  public countryCode!: string | null;
+  public phoneNumber!: string;
+  public theme!: string;
+  public password!: string;
+  public timeZone!: string | null;
+  public lastLogin!: number | null;
+
+  // timestamps!
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+const UserModel = (sequelize: Sequelize): typeof User => {
+  User.init({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    fullName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    accountName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    userType: {
+      type: DataTypes.STRING,
+      defaultValue: 'user',
+    },
+    profilePicture: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    coverPhoto: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    countryCode: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    theme: {
+      type: DataTypes.STRING,
+      defaultValue: 'system',
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    timeZone: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    lastLogin: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+  }, {
+    sequelize,
+    tableName: 'users',
+    freezeTableName: true,
+    timestamps: true,
+    indexes: [{
+      unique: false,
+      fields: ['phoneNumber'],
+      name: 'phoneNumber_index',
+    }],
+  });
+
+  return User;
+};
 
 export default UserModel;
